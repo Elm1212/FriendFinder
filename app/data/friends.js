fbbc966f06010fc41bb2a061f66478733cb5746e
Elm <emmal.muniz@gmail.com>
2019-01-24 11:43:36 -0600

    adding info, routes still not working

---
+++ app/data/friends.js
@@ -0,0 +1,76 @@
+var  friendsArray = [
+    {
+        "name": "Stevie Wonder",
+        "photo": 
+        "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiLsZLus4LgAhURbK0KHQg5DUQQjRx6BAgBEAU&url=http%3A%2F%2Fsing.wikia.com%2Fwiki%2FFile%3AStevie_Wonder.jpg&psig=AOvVaw3fo3gYCmxP-YBko6R2hW3l&ust=1548281411984544",
+        "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+        },
+    
+    {
+        "name": "Betty Davis",
+        "photo": 
+        "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiFq-iXtILgAhVSd6wKHR8RAj8QjRx6BAgBEAU&url=https%3A%2F%2Fpitchfork.com%2Fnews%2F61232-betty-davis-film-nasty-gal-seeks-funding%2F&psig=AOvVaw39pv4AkkT20WQVr1gJYT_c&ust=1548281491995078",
+        "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+        },
+
+    {
+        "name": "Dolly Parton",
+        "photo": 
+        "https://www.google.com/search?q=dolly+parton&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiq_73-3obgAhVNVK0KHfhyASEQ_AUIDigB&biw=1440&bih=790#imgdii=CgcbPtHxZfOkOM:&imgrc=pLJsLZOp4c27-M:",
+        "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+        },
+
+    {
+        "name": "Elvis Presley",
+        "photo": 
+        "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwimz8fjtILgAhUGP60KHa59CBoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.rottentomatoes.com%2Fcelebrity%2Felvis_presley&psig=AOvVaw0kCzxIvB8uGMZiGv5FVDJf&ust=1548281651981007",
+        "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+        }
+    ];
+
+    // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
